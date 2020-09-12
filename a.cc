7fd44dd1a8b6e66f10b50d0f514230b9b9deb9b8
garyhsu <garyhsu1209@gmail.com>
2020-09-12 13:42:43 +0800

    add hanoi

--- a.cc
+++ a.cc
@@ -1,6 +1,10 @@
 #include <cstdio>
 
-void hanoi(int src, int dst, 
+void hanoi(int src, int dst, int mid, int n){
+  if(n == 1){
+    puts("Move One Plate from %d to %d\n", src, dst);
+  }
+} 
 
 int main(){
   int a = 5;
